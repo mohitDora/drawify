021e2277f0a170cce109e2c10c5c6f7311a0b4a8
mohitDora <doramohitkumar@gmail.com>
2024-09-01 16:16:52 +0530

    ch-6

--- client/app/board/[boardId]/page.js
+++ client/app/board/[boardId]/page.js
@@ -1,21 +1,25 @@
 "use client";
-import Canvas from '@/components/shared/Canvas';
-import { addUserToBoard } from '@/lib/ApiFunction';
-import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
-import { useParams, useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import Canvas from "@/components/shared/Canvas";
+import { addUserToBoard } from "@/lib/ApiFunction";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const BoardPage = () => {
-  const {user} = useKindeBrowserClient();
+  const { user } = useKindeBrowserClient();
   const { boardId } = useParams();
   const router = useRouter();
 
   useEffect(() => {
     if (!user) {
-      router.push(`/login?redirect=/board/${boardId}`);
-      addUserToBoard(boardId,user?.id)
-    }else{
-      addUserToBoard(boardId,user?.id)
+      const kindeIssuerUrl = process.env.KINDE_ISSUER_URL;
+      const clientId = process.env.KINDE_CLIENT_ID;
+      const redirectUri = process.env.KINDE_REDIRECT_URI;
+      const loginUrl = `${kindeIssuerUrl}/oauth2/auth?client_id=${clientId}&response_type=code&scope=openid profile email&redirect_uri=${redirectUri}`;
+      window.location.href = loginUrl;
+      addUserToBoard(boardId, user?.id);
+    } else {
+      addUserToBoard(boardId, user?.id);
     }
   }, [user, boardId, router]);
 
@@ -25,7 +29,7 @@ const BoardPage = () => {
 
   return (
     <div>
-      <p>{user?.given_name+" "+user?.family_name}</p>
+      <p>{user?.given_name + " " + user?.family_name}</p>
       <h1>Board: {boardId}</h1>
       <Canvas boardId={boardId} />
     </div>
