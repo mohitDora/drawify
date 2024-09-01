0ab1b483ee44849b0fcfb34223839e6e6f104475
mohitDora <doramohitkumar@gmail.com>
2024-09-01 15:01:17 +0530

    changes

--- client/lib/ApiFunction.js
+++ client/lib/ApiFunction.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import { toast } from 'sonner';
 
-const API_URL = process.env.BASE_URL+"/api"
+const API_URL = process.env.BASE_URL
 
 // User API Calls
 
 export const registerUser = async (userData) => {
   try {
-    const response = await axios.post(`${API_URL}/users/`, userData);
+    const response = await axios.post(`${API_URL}/api/users/`, userData);
     toast("Authentication Successful")
     return response.data;
   } catch (error) {
@@ -17,7 +17,7 @@ export const registerUser = async (userData) => {
 
 export const fetchUser = async (userId) => {
   try {
-    const response = await axios.get(`${API_URL}/users/${userId}`);
+    const response = await axios.get(`${API_URL}/api/users/${userId}`);
     return response.data;
   } catch (error) {
     throw error.response ? error.response.data : new Error('Network Error');
@@ -28,7 +28,7 @@ export const fetchUser = async (userId) => {
 
 export const createBoard = async (boardData) => {
   try {
-    const response = await axios.post(`${API_URL}/boards`, boardData, {
+    const response = await axios.post(`${API_URL}/api/boards`, boardData, {
       headers: {
         'Content-Type': 'application/json',
       },
@@ -43,7 +43,7 @@ export const createBoard = async (boardData) => {
 
 export const fetchBoards = async (uid) => {
   try {
-    const response = await axios.get(`${API_URL}/boards/${uid}`);
+    const response = await axios.get(`${API_URL}/api/boards/${uid}`);
     console.log(response)
     return response.data;
   } catch (error) {
@@ -53,7 +53,7 @@ export const fetchBoards = async (uid) => {
 
 export const fetchBoardById = async (boardId) => {
   try {
-    const response = await axios.get(`${API_URL}/boards/board/${boardId}`);
+    const response = await axios.get(`${API_URL}/api/boards/board/${boardId}`);
     return response.data;
   } catch (error) {
     throw error.response ? error.response.data : new Error('Network Error');
@@ -62,7 +62,7 @@ export const fetchBoardById = async (boardId) => {
 
 export const addUserToBoard = async (boardId, userId) => {
   try {
-    const response = await axios.post(`${API_URL}/boards/${boardId}/addUser`, { userId });
+    const response = await axios.post(`${API_URL}/api/boards/${boardId}/addUser`, { userId });
     return response.data;
   } catch (error) {
     throw error.response ? error.response.data : new Error('Network Error');
@@ -71,7 +71,7 @@ export const addUserToBoard = async (boardId, userId) => {
 
 export const updateBoard = async (boardId, boardData) => {
     try {
-      const response = await axios.put(`${API_URL}/boards/${boardId}`, boardData);
+      const response = await axios.put(`${API_URL}/api/boards/${boardId}`, boardData);
       return response.data;
     } catch (error) {
       throw error.response ? error.response.data : new Error('Network Error');
